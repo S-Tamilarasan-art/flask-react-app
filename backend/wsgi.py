7525491a21918717d56231765111df76823ea6b0
tamilarasant904@gmail.com <tamilarasant904@gmail.com>
2025-11-09 17:29:51 +0530

    wsgi.py was changed due to bugs

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,5 @@
 
-from app import app  # Replace 'app' with the name of your main Flask app file if different
+from main import app  # Replace 'app' with the name of your main Flask app file if different
 
 if __name__ == "__main__":
     app.run()
