047b731af845927c50b4d981a81edb4d26779e3a
tamilarasant904@gmail.com <tamilarasant904@gmail.com>
2025-11-14 14:29:10 +0530

    env variable changed

--- backend/main.py
+++ backend/main.py
@@ -16,17 +16,12 @@ app = Flask(__name__)
 
 frontend_dev = os.getenv("FRONTEND_DEV_URL")
 frontend_prod = os.getenv("FRONTEND_PROD_URL")
-db_url = os.getenv("DATABASE_URL") or os.getenv("MYSQL_URL")
+raw_url = os.getenv("MYSQL_PUBLIC_URL") 
 
-if not db_url:
-    # Manually build connection if Railway splits it
-    host = os.getenv("MYSQLHOST")
-    user = os.getenv("MYSQLUSER")
-    password = os.getenv("MYSQLPASSWORD")
-    dbname = os.getenv("MYSQLDATABASE")
-    port = os.getenv("MYSQLPORT", 3306)
-    if host and user and password and dbname:
-        db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
+
+print(raw_url)
+
+   
 # CORS configuration
 CORS(app, resources={
     r"/signup/*": {"origins": [frontend_dev, frontend_prod]},
@@ -34,15 +29,10 @@ CORS(app, resources={
     r"/dashboard/*": {"origins": [frontend_dev, frontend_prod]},
 })
 
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_DATABASE_URI'] = raw_url
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
-print("MYSQL_URL:", os.getenv("MYSQL_URL"))
-print("MYSQLHOST:", os.getenv("MYSQLHOST"))
-print("MYSQLUSER:", os.getenv("MYSQLUSER"))
-print("MYSQLPASSWORD:", os.getenv("MYSQLPASSWORD"))
-print("MYSQLDATABASE:", os.getenv("MYSQLDATABASE"))
-print("MYSQLPORT:", os.getenv("MYSQLPORT"))
+
 
 @app.after_request
 def add_cors_headers(response):
@@ -61,7 +51,8 @@ app.register_blueprint(dashboard_bp,url_prefix='/dashboard')
 
 with app.app_context():
     db.create_all()
-
+    db.session.close()
+    db.engine.dispose()
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,5 +7,5 @@ load_dotenv()
 from main import app  # Import AFTER .env is loaded
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run()
